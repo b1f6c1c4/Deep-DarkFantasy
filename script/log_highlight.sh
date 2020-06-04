8cd80fb7d635d74bffa94e4ede801b0060029a75
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-04 01:50:34 -0400

    better log

--- script/log_highlight.sh
+++ script/log_highlight.sh
@@ -4,6 +4,12 @@ awk '
     /^# / { printf "\033[36m%s\033[0m\n", $0; }
     /^ERROR:/ { printf "\033[31m%s\033[0m\n", $0; }
     /^CRITICAL WARNING:/ { printf "\033[35m%s\033[0m\n", $0; }
-    /^WARNING:/ { printf "\033[33m%s\033[0m\n", $0; }
+    /^WARNING:/ {
+      if (match($0, /\[Synth 8-689\]/)) {
+        printf "\033[31m%s\033[0m\n", $0;
+      } else {
+        printf "\033[33m%s\033[0m\n", $0;
+      }
+    }
     /^Resolution:/ { print $0; }
 '
