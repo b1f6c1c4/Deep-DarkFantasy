777c0ea783241c4aa1ed46bcb0b1d1cdb026363e
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-12 22:40:24 -0400

    zybo io

--- script/program.tcl
+++ script/program.tcl
@@ -1,12 +1,12 @@
 open_hw_manager
 connect_hw_server -url localhost:3121
-current_hw_target [get_hw_targets */xilinx_tcf/Digilent/*]
+current_hw_target [get_hw_targets */xilinx_tcf/Digilent/210351A82051A]
 open_hw_target
 
-current_hw_device [get_hw_devices xc7z100_1]
-refresh_hw_device -update_hw_probes false [get_hw_devices xc7z100_1]
-set_property PROGRAM.FILE {output.bit} [get_hw_devices xc7z100_1]
-# set_property PROBES.FILE {output.ltx} [get_hw_devices xc7z100_1]
+current_hw_device [get_hw_devices xc7z020_1]
+refresh_hw_device -update_hw_probes false [get_hw_devices xc7z020_1]
+set_property PROGRAM.FILE {output.bit} [get_hw_devices xc7z020_1]
+# set_property PROBES.FILE {output.ltx} [get_hw_devices xc7z020_1]
 
-program_hw_devices [get_hw_devices xc7z100_1]
-refresh_hw_device [get_hw_devices xc7z100_1]
+program_hw_devices [get_hw_devices xc7z020_1]
+refresh_hw_device [get_hw_devices xc7z020_1]
