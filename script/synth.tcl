3c616439f02e6b8dedf4d39e3a41b56c331af31e
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-12 22:40:24 -0400

    zybo io

--- script/synth.tcl
+++ script/synth.tcl
@@ -1,20 +1,22 @@
 set_param general.maxThreads 32
 
-set_part xc7z100ffg900-2
+set_part xc7z020clg400-1
 
 read_verilog [glob ../design/*.v]
-read_verilog [glob ../design/ip/i2c_master/*.v]
 
-file mkdir ./ip/sys_pll
-file copy -force ../ip/sys_pll.xci ./ip/sys_pll/
-read_ip ./ip/sys_pll/sys_pll.xci
-set locked [get_property IS_LOCKED [get_ips sys_pll]]
-set upgrade [get_property UPGRADE_VERSIONS [get_ips sys_pll]]
-if {$upgrade != "" && $locked} {
-    upgrade_ip [get_ips sys_pll]
-}
-generate_target all [get_ips sys_pll]
-synth_ip [get_ips sys_pll]
+# set_property ip_repo_paths ../vivado-library [current_project]
+# update_ip_catalog
+
+# file mkdir ./ip/sys_pll
+# file copy -force ../ip/sys_pll.xci ./ip/sys_pll/
+# read_ip ./ip/sys_pll/sys_pll.xci
+# set locked [get_property IS_LOCKED [get_ips sys_pll]]
+# set upgrade [get_property UPGRADE_VERSIONS [get_ips sys_pll]]
+# if {$upgrade != "" && $locked} {
+#     upgrade_ip [get_ips sys_pll]
+# }
+# generate_target all [get_ips sys_pll]
+# synth_ip [get_ips sys_pll]
 
 set VIN_FREQ $::env(FREQ)
 read_xdc [glob ../constr/zybo-z7-20.xdc]
