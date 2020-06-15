d68358e52341fe6921c0db16915bb25d52f72152
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-06-14 22:31:09 -0400

    WIP: simpl

--- design/blk_buffer.v
+++ design/blk_buffer.v
@@ -1,57 +1,29 @@
 module blk_buffer #(
-   parameter HP = 1920,
-   parameter ML = 192,
-   parameter KH = 10,
+   parameter BLKS = 10,
    parameter MAX = 2
 ) (
    input clk_i,
-   input vp_last_i,
-   input hs_i,
-   input hs_r_i,
+   input [31:0] tile_i,
+   input vs_i,
+   input vs_r_i,
    input de_i,
-   input de_r_i,
-   input [7:0] wd_i,
+   input [2:0] wd_i,
    output reg rx_o
 );
-   localparam BLKS = (HP + KH - 1) / KH;
-   localparam DEPTH = $clog2(MAX) + 1;
-   localparam BD = $clog2(BLKS) + 1;
-   localparam MD = $clog2(ML) + 1;
+   localparam DEPTH = $clog2(MAX);
+   localparam THRES = MAX >= 4096 ? (~32'h1ff & MAX / 2) : MAX / 2;
 
-   reg [MD-1:0] ml_cnt;
-   always @(posedge clk_i) begin
-      if (hs_i && ~hs_r_i) begin
-         ml_cnt <= ML - 1;
-      end else if (~de_i && de_r_i) begin
-         ml_cnt <= 0;
-      end else if (|ml_cnt) begin
-         ml_cnt <= ml_cnt - 1;
-      end
-   end
-
-   reg [BD-1:0] h_cur, hb_cur;
-   always @(posedge clk_i) begin
-      if (|ml_cnt) begin
-         h_cur <= 0;
-         hb_cur <= 0;
-      end else if (h_cur == KH-1) begin
-         h_cur <= 0;
-         hb_cur <= hb_cur + 1;
-      end else begin
-         h_cur <= h_cur + 1;
-      end
-   end
-
-   wire [DEPTH-1:0] buf_a[0:BLKS-1];
+   wire buf_a[0:BLKS-1];
    genvar i;
    generate
       for (i = 0; i < BLKS; i = i + 1) begin : g
          double_buffer #(
-            .DEPTH (DEPTH)
+            .DEPTH (DEPTH),
+            .THRES (THRES)
          ) i_double_buffer (
             .clk_i,
-            .freeze_i (vp_last_i && ~de_i && de_r_i),
-            .de_i (de_i && i == hb_cur),
+            .freeze_i (~vs_r_i && vs_i),
+            .de_i (de_i && i == tile_i),
             .wd_i,
             .buf_a (buf_a[i])
          );
@@ -59,7 +31,7 @@ module blk_buffer #(
    endgenerate
 
    always @(posedge clk_i) begin
-      rx_o <= buf_a[hb_cur] >= MAX / 2;
+      rx_o <= buf_a[tile_i];
    end
 
 endmodule
--- design/double_buffer.v
+++ design/double_buffer.v
@@ -1,17 +1,18 @@
 module double_buffer #(
-   parameter DEPTH = 1
+   parameter DEPTH = 1,
+   parameter THRES = 0
 ) (
    input clk_i,
    input freeze_i,
    input de_i,
-   input [7:0] wd_i,
-   output reg [DEPTH-1:0] buf_a
+   input [2:0] wd_i,
+   output reg buf_a
 );
 
    reg [DEPTH-1:0] buf_b;
    always @(posedge clk_i) begin
       if (freeze_i) begin
-         buf_a <= buf_b;
+         buf_a <= (buf_b > THRES);
          buf_b <= 0;
       end else if (de_i) begin
          buf_b <= buf_b + {1'b0,wd_i};
