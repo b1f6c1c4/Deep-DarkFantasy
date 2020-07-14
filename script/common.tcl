b302ee3ebe38f003a10b03b8ef4c4c94f6026bc8
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-07-14 02:51:25 -0400

    hide useless info

--- script/common.tcl
+++ script/common.tcl
@@ -1,13 +1,18 @@
 set_param general.maxThreads 8
 
+set_msg_config -id {[Vivado 12-2489]} -string "-input_jitter" -new_severity INFO
 set_msg_config -id {[IP_Flow 19-4965]} -string "ila_" -new_severity INFO
 set_msg_config -id {[Synth 8-7023]} -string "processing_system7" -new_severity INFO
 set_msg_config -id {[Synth 8-3331]} -string "processing_system7" -new_severity INFO
+set_msg_config -id {[Synth 8-3331]} -string "has unconnected port m_axi_" -new_severity INFO
+set_msg_config -id {[Synth 8-3331]} -string "has unconnected port sw_i" -new_severity INFO
 set_msg_config -id {[Constraints 18-5210]} -new_severity INFO
 set_msg_config -id {[Timing 38-164]} -new_severity INFO
 set_msg_config -id {[Timing 38-127]} -string "sys_clk_pin" -new_severity INFO
 set_msg_config -id {[Constraints 18-550]} -string "IBUF_LOW_PWR" -new_severity INFO
 set_msg_config -id {[DRC REQP-1839]} -string "RAM" -new_severity INFO
+set_msg_config -id {[DRC DPIP-1]} -string "pipelining: DSP" -new_severity INFO
+set_msg_config -id {[DRC DPOP-2]} -string "pipelining: DSP" -new_severity INFO
 set_msg_config -id {[Vivado 12-750]} -new_severity INFO
 set_msg_config -string "ila_pixclk" -new_severity INFO
 set_msg_config -string "ila_refclk" -new_severity INFO
