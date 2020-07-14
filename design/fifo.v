28ffaebf7999209b04ce104307be4e961bb62a67
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-07-14 16:23:48 -0400

    fix critical bug using fifo36

--- design/fifo.v
+++ design/fifo.v
@@ -13,9 +13,13 @@ module fifo #(
    output [WIDTH-1:0] out_data_o,
    input out_rdy_i
 );
+   localparam PIECES = (WIDTH + 32 - 1) / 32;
 
-   wire full, empty, aempty;
-   wire [63:0] rdata;
+   wire [32*PIECES-1:0] wdata, rdata;
+   wire [PIECES-1:0] fulls, emptys, aemptys;
+   wire full = |fulls;
+   wire empty = |emptys;
+   wire aempty = |aemptys;
    reg rden;
 
    reg rbuf, rbuf_next;
@@ -24,38 +28,47 @@ module fifo #(
    end
 
    assign in_rdy_o = ~full;
+   assign wdata = in_data_i;
    assign out_data_o = rdata[WIDTH-1:0];
 
-   FIFO36E1 #(
-      .ALMOST_FULL_OFFSET (1),
-      .ALMOST_EMPTY_OFFSET (BURST > 0 ? BURST - 1 : 1),
-      .EN_SYN ("TRUE"),
-      .DATA_WIDTH (WIDTH <= 8 ? 9 : WIDTH <= 16 ? 18 : WIDTH <= 32 ? 36 : 72),
-      .DO_REG (0)
-   ) inst (
-      .RST (srst_i),
-      .RSTREG (0),
-      .REGCE (0),
+   genvar i;
+   generate
+      for (i = 0; i < PIECES; i = i + 1) begin : g
+         wire [32:0] w = wdata[i*32+31:i*32];
+         wire [63:0] ro;
+         assign rdata[i*32+31:i*32] = ro[31:0];
+         FIFO36E1 #(
+            .ALMOST_FULL_OFFSET (1),
+            .ALMOST_EMPTY_OFFSET (BURST > 0 ? BURST - 1 : 1),
+            .EN_SYN ("TRUE"),
+            .DATA_WIDTH (36),
+            .DO_REG (0)
+         ) inst (
+            .RST (srst_i),
+            .RSTREG (0),
+            .REGCE (0),
 
-      .WRCLK (clk_i),
-      .WREN (~srst_i && in_val_i && in_rdy_o),
-      .DI (in_data_i),
-      .DIP (0),
-      .FULL (full),
-      .ALMOSTFULL (),
-      .WRCOUNT (), .WRERR (),
+            .WRCLK (clk_i),
+            .WREN (~srst_i && in_val_i && in_rdy_o),
+            .DI ({32'b0,w[31:0]}),
+            .DIP (0),
+            .FULL (fulls[i]),
+            .ALMOSTFULL (),
+            .WRCOUNT (), .WRERR (),
 
-      .RDCLK (clk_i),
-      .RDEN (rden),
-      .DO (rdata),
-      .DOP (),
-      .EMPTY (empty),
-      .ALMOSTEMPTY (aempty),
-      .RDCOUNT (), .RDERR (),
+            .RDCLK (clk_i),
+            .RDEN (rden),
+            .DO (ro),
+            .DOP (),
+            .EMPTY (emptys[i]),
+            .ALMOSTEMPTY (aemptys[i]),
+            .RDCOUNT (), .RDERR (),
 
-      .INJECTDBITERR (0), .INJECTSBITERR (0),
-      .DBITERR (), .SBITERR (), .ECCPARITY ()
-   );
+            .INJECTDBITERR (0), .INJECTSBITERR (0),
+            .DBITERR (), .SBITERR (), .ECCPARITY ()
+         );
+      end
+   endgenerate
 
    always @(*) begin
       if (srst_i) begin
